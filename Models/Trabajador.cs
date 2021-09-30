c1fb916bd2b27613a176ced5ad8a54e1b2a4b3ea
dandy160289 <drufino.dijisa@gmail.com>
2021-09-30 09:51:15 -0500

    Add project files.

---
+++ Models/Trabajador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTrabajadoresPrueba.Models
+{
+    public class Trabajador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Tipo { get; set; }
+
+        [Required]
+        public string Numero { get; set; }
+
+        [Required]
+        public string Nombres { get; set; }
+
+        [Required]
+        public string Sexo { get; set; }
+
+        [Required]
+        public int IdDep { get; set; }
+
+        [Required]
+        public int IdPro { get; set; }
+
+        [Required]
+        public int IdDis { get; set; }
+    }
+}
